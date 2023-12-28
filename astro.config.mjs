36c9e3d42911f1babcc205093200dee8b6d3ca14
Manuel Del Castillo <manuel.delcastillo@walmeric.com>
2023-12-28 13:48:57 +0100

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  alias: {
+    // Alias para archivos estáticos en la carpeta public
+    '@public': './public',
+  },
+  integrations: [tailwind()]
+});
